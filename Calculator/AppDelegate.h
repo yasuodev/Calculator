ab593e6d0430fafd038755b2518b812ff4babdd2
Yasuo <yasuodev@protonmail.com>
2017-05-22 11:37:35 +0900

    Initial commit.

---
+++ Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Calculator
+//
+//  Created by Dev on 5/22/17.
+//  Copyright © 2017 Dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Calculator
+//
+//  Created by Dev on 5/22/17.
+//  Copyright © 2017 Dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
